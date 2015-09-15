3545bced404a5a877948fee14204e9b3b9f53731
Your Name <cbergenhem@gmail.com>
2015-09-14 21:12:35 -0400

    Set up basic jasmine test

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Yass');
+app.get("/", function (req, res) {
+  res.send("Hello World");
 });
 
 var server = app.listen(3000, function () {
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', "localhost", port);
-
+  console.log("Example app listening at http://%s:%s", "localhost", port);
 });
--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -4,10 +4,19 @@ var baseUrl = "http://localhost:3000";
 
 describe("Hello World Server", function() {
   describe("GET /", function() {
-    it("returns status code 200", function() {
+    it("returns status code 200", function(done) {
       request.get(baseUrl, function(error, response, body) {
         expect(response.statusCode).toBe(200);
+        done();
       });
     });
+
+    it("returns 'Hello World'", function(done) {
+      request.get(baseUrl, function(err, response, body) {
+        expect(body).toBe("Hello World");
+        done();
+      });
+    });
+
   });
 });
