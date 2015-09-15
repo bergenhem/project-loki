de02ca9c977e142dbe6e88dbc6ee35c6920396b5
Your Name <cbergenhem@gmail.com>
2015-09-14 21:45:29 -0400

    Add test for getting provinces

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -2,7 +2,7 @@ var request = require("request");
 
 var baseUrl = "http://localhost:3000";
 
-describe("Hello World Server", function() {
+describe("Server", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(baseUrl, function(error, response, body) {
@@ -18,5 +18,20 @@ describe("Hello World Server", function() {
       });
     });
 
+    describe("GET all provinces", function() {
+      it("returns status code 200", function(done) {
+        request.get(baseUrl + "/provinces", function(error, response, body) {
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+      });
+
+      it("returns array of provinces", function(done) {
+        expect(response.provinces.length).not.toBe(0);
+        expect(response.provinces).not.toBe(undefined);
+        expect(response.provinces).not.toBe(null);
+        done();
+      });
+    });
   });
 });
